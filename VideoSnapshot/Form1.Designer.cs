cd62858de4bea9e6727f2168200af7a5bf9983d2
Ptarmigans <saptarmigans@gmail.com>
2018-04-29 14:24:09 +0100

    Initial commit.

---
+++ VideoSnapshot/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace VideoScreenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.start = new System.Windows.Forms.Button();
+            this.addFiles = new System.Windows.Forms.Button();
+            this.removeFiles = new System.Windows.Forms.Button();
+            this.currentProgress = new System.Windows.Forms.ProgressBar();
+            this.totalProgress = new System.Windows.Forms.ProgressBar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(279, 95);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 0;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(172, 163);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(118, 29);
+            this.start.TabIndex = 2;
+            this.start.Text = "Start!";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // addFiles
+            // 
+            this.addFiles.Location = new System.Drawing.Point(12, 113);
+            this.addFiles.Name = "addFiles";
+            this.addFiles.Size = new System.Drawing.Size(32, 21);
+            this.addFiles.TabIndex = 3;
+            this.addFiles.Text = "+";
+            this.addFiles.UseVisualStyleBackColor = true;
+            this.addFiles.Click += new System.EventHandler(this.addFiles_Click);
+            // 
+            // removeFiles
+            // 
+            this.removeFiles.Location = new System.Drawing.Point(50, 113);
+            this.removeFiles.Name = "removeFiles";
+            this.removeFiles.Size = new System.Drawing.Size(32, 21);
+            this.removeFiles.TabIndex = 4;
+            this.removeFiles.Text = "-";
+            this.removeFiles.UseVisualStyleBackColor = true;
+            this.removeFiles.Click += new System.EventHandler(this.removeFiles_Click);
+            // 
+            // currentProgress
+            // 
+            this.currentProgress.Location = new System.Drawing.Point(12, 212);
+            this.currentProgress.Name = "currentProgress";
+            this.currentProgress.Size = new System.Drawing.Size(278, 19);
+            this.currentProgress.TabIndex = 5;
+            // 
+            // totalProgress
+            // 
+            this.totalProgress.Location = new System.Drawing.Point(12, 256);
+            this.totalProgress.Name = "totalProgress";
+            this.totalProgress.Size = new System.Drawing.Size(278, 19);
+            this.totalProgress.Step = 1;
+            this.totalProgress.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 165);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(103, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.Value = new System.DateTime(2018, 4, 29, 0, 5, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Time between captures";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Current Progress";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Total Progress";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 287);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.totalProgress);
+            this.Controls.Add(this.currentProgress);
+            this.Controls.Add(this.removeFiles);
+            this.Controls.Add(this.addFiles);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Video Frame Capture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button addFiles;
+        private System.Windows.Forms.Button removeFiles;
+        private System.Windows.Forms.ProgressBar currentProgress;
+        private System.Windows.Forms.ProgressBar totalProgress;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ VideoSnapshot/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoScreenshot
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource cts;
+        public Form1()
+        {
+            InitializeComponent();
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+        }
+
+        private void listBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string ext;
+            int i;
+            for (i = 0; i < files.Length; i++)
+            {
+                ext = Path.GetExtension(files[i]);
+                //Filter out directories
+                if (ext != "")
+                    listBox1.Items.Add(files[i]);
+            }
+        }
+
+        private void addFiles_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        private void removeFiles_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedObjectCollection selection = new ListBox.SelectedObjectCollection(listBox1);
+            selection = listBox1.SelectedItems;
+
+            while (listBox1.SelectedIndex >= 0)
+            {
+                int i;
+                for (i = selection.Count - 1; i >= 0; i--)
+                {
+                    listBox1.Items.Remove(selection[i]);
+                }
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Stop");
+            if(cts != null)
+            {
+                cts.Cancel();
+            }
+            unlockControls();
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            try
+            {
+                await started(cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Cancelled");
+            }
+        }
+
+        private async Task started(CancellationToken ct)
+        {
+            lockDownControls();
+            currentProgress.Value = 0;
+            totalProgress.Value = 0;
+            
+            //Get all the files for processing
+            ListBox.ObjectCollection files = new ListBox.ObjectCollection(listBox1);
+            files = listBox1.Items;
+            int filesNo = files.Count;
+            int processedFrames = 0;
+            int processedFiles = 0;
+
+            //Stop if there are no files
+            if (filesNo == 0)
+            {
+                MessageBox.Show("No videos selected!");
+                return;
+            }
+
+            string arg;
+            string location = "";
+            double[] lengthSecs = new double[filesNo];
+            //Get the time in seconds for the interval from the date time picker
+            double interval = dateTimePicker1.Value.TimeOfDay.TotalSeconds;
+            string stdOut = "";
+            string timeFromSeconds;
+            string timeFromSecondsFileSafe;
+
+            //Set up conditions for running ffmpeg
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            //Don't show the cmd window
+            p.StartInfo.CreateNoWindow = true;
+            //Hang onto the shell output
+            p.StartInfo.RedirectStandardOutput = true;
+
+            //Get the lengths of all the videos
+            for (int i = 0; i < filesNo; i++)
+            {
+                location = files[i].ToString();
+                arg = "/C ffmpeg -i \"" + location + "\" 2>&1 | find \"Duration\"";
+                p.StartInfo.Arguments = arg;
+                p.Start();
+                stdOut = p.StandardOutput.ReadToEnd();
+                lengthSecs[i] = TimeSpan.Parse(stdOut.Substring(12, 11)).TotalSeconds;
+            }
+            double currVal;
+            totalProgress.Maximum = filesNo;
+            for (int i = 0; i < filesNo; i++)
+            {
+                location = files[i].ToString();
+                processedFiles++;
+                for (double j = 0; j < lengthSecs[i]; j = j + interval)
+                {
+                    timeFromSeconds = TimeSpan.FromSeconds(j).ToString();
+                    timeFromSecondsFileSafe = string.Format("{0:hh\\-mm\\-ss}", TimeSpan.FromSeconds(j));
+                    arg = "/C ffmpeg -y -ss " + timeFromSeconds + " -i \"" + location + "\" -frames:v 1 \"" + Path.GetDirectoryName(location) + "\\" + Path.GetFileNameWithoutExtension(location) + "-" + timeFromSecondsFileSafe + ".png\"";
+                    //p.StartInfo.Arguments = arg;
+                    Console.WriteLine(arg);
+                    //p.Start();
+                    //p.WaitForExit();
+                    await waitForExitAsync(arg);
+                    processedFrames++;
+                    currVal = (j / lengthSecs[i]) * 100;
+                    currentProgress.Value = (int)currVal;
+                    if (ct.IsCancellationRequested)
+                    {
+                        MessageBox.Show(processedFrames + " frames processed from " + processedFiles + " file(s) out of " + filesNo + ".");
+                        currentProgress.Value = 0;
+                        totalProgress.Value = 0;
+                        return;
+                    }
+                }
+                totalProgress.PerformStep();
+            }
+            totalProgress.Value = totalProgress.Maximum;
+            currentProgress.Value = currentProgress.Maximum;
+            MessageBox.Show("Done! " + processedFrames + " frames processed from " + processedFiles + " file(s) out of " + filesNo + ".");
+            unlockControls();
+        }
+
+        private Task waitForExitAsync(string arg)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            //Don't show the cmd window
+            p.StartInfo.CreateNoWindow = true;
+            //Hang onto the shell output
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.Arguments = arg;
+            p.EnableRaisingEvents = true;
+            p.Exited += (sender, args) =>
+            {
+                tcs.SetResult(true);
+                p.Dispose();
+            };
+            p.Start();
+            return tcs.Task;
+        }
+
+        private void lockDownControls()
+        {
+            //Disable + button, - button, drag and drop, changing timer, change start to stop
+            addFiles.Enabled = false;
+            removeFiles.Enabled = false;
+            listBox1.AllowDrop = false;
+            dateTimePicker1.Enabled = false;
+            //Change the start button to act as a stop button
+            start.Text = "Stop!";
+            start.Click -= start_Click;
+            start.Click += stop_Click;
+        }
+
+        private void unlockControls()
+        {
+            addFiles.Enabled = true;
+            removeFiles.Enabled = true;
+            listBox1.AllowDrop = true;
+            dateTimePicker1.Enabled = true;
+            start.Text = "Start!";
+            start.Click -= stop_Click;
+            start.Click += start_Click;
+        }
+    }
+}
